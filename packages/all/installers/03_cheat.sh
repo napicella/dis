779727a99f7021c0929a89373bd60dfc8ef6f28b
Nicola <napicella4@gmail.com>
2026-05-25 20:43:31 +0200

    Fix hardcoded path in cheat package installer

--- packages/all/installers/03_cheat.sh
+++ packages/all/installers/03_cheat.sh
@@ -5,7 +5,7 @@
 ### -- End
 source $DIS_BINDING
 
- go install github.com/cheat/cheat/cmd/cheat@latest
+go install github.com/cheat/cheat/cmd/cheat@latest
 
 echo "Rendering the cheat configuration"
 config_path="$DIS_CONFIG_FOLDER/cheat/conf.yml"
@@ -14,5 +14,11 @@ envsubst < "$config_path" > /tmp/cheat_rendered.yml
 echo "Installing the cheat configuration"
 mkdir -p $HOME/.config/cheat
 mv /tmp/cheat_rendered.yml $HOME/.config/cheat/conf.yml
+
+# Initialize community and work channels
+if [ ! -d $HOME/.config/cheat/cheatsheets/community ]; then
+    git clone https://github.com/cheat/cheatsheets $HOME/.config/cheat/cheatsheets/community
+fi
+mkdir -p $HOME/.config/cheat/cheatsheets/work
 
 echo "Done"
