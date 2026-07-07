2fcc7313077acd4eb12cd30c73968edf74d5f21a
Nicola <napicella4@gmail.com>
2026-07-08 01:20:05 +0200

    Ulauncher installs community themes

--- packages/ubuntu/installers/gui/02_ulauncher.sh
+++ packages/ubuntu/installers/gui/02_ulauncher.sh
@@ -36,3 +36,12 @@ cp $DIS_CONFIG_FOLDER/ulauncher.desktop ~/.config/autostart/ulauncher.desktop
 gtk-launch ulauncher.desktop >/dev/null 2>&1
 sleep 2 # ensure enough time for ulauncher to set defaults
 cp $DIS_CONFIG_FOLDER/ulauncher.json ~/.config/ulauncher/settings.json
+
+# Install community themes: https://docs.ulauncher.io/en/latest/themes/themes.html
+mkdir -p ~/.config/ulauncher/user-themes
+git clone https://github.com/LucianoBigliazzi/ulauncher-nord.git ~/.config/ulauncher/user-themes/ulauncher-nord
+git clone https://github.com/sociale11/ul-cosmo ~/.config/ulauncher/user-themes/ul-cosmo
+git clone https://github.com/hmwassim/WhiteSur-Nord-ulauncher.git ~/.config/ulauncher/user-themes/WhiteSur-Nord-ulauncher
+git clone https://github.com/SirHades696/TokyoNight-Ulauncher-Theme /tmp/TokyoNight-Ulauncher-Theme && cp -r /tmp/TokyoNight-Ulauncher-Theme/TokyoNight ~/.config/ulauncher/user-themes/
+mkdir -p ~/.config/ulauncher/user-themes/Viridian git clone https://github.com/arthurrio/ulauncher-viridian-theme ~/.config/ulauncher/user-themes/Viridian
+git clone https://github.com/napicella/Matcha-Dark-Aliz-ulauncher.git ~/.config/ulauncher/user-themes/Matcha-Dark-Aliz-ulauncher
