5900868828710afe5b6d3ef761356f56dba7c400
napicell <napicell@amazon.com>
2026-09-10 11:40:23 +0200

    Update tf installer

--- packages/ubuntu/installers/03_thefuck.sh
+++ packages/ubuntu/installers/03_thefuck.sh
@@ -10,11 +10,11 @@ if [[ -n "${DIS_INSTALL:-}" ]]; then
 
   # On Debian-based distros, manual activation is required.
   # The following adds the activation in the bash init which is included in bashrc.
-  IMPORT_LINES='if command -v thefuck &> /dev/null
+  dis tools add-rc-init \
+    --name 'TheFuck' \
+    --content 'if command -v thefuck &> /dev/null
 then
   eval $(thefuck --alias please)
   eval $(thefuck --alias fuck)
 fi'
-
-  bashrc_init_add "TheFuck" "$IMPORT_LINES"
 fi
