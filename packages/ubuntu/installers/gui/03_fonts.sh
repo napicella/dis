983f30353ce6a6294b7ea321f0395ebf666eab24
napicell <napicell@amazon.com>
2026-09-10 11:25:10 +0200

    Fix fonts selector

--- packages/ubuntu/installers/gui/03_fonts.sh
+++ packages/ubuntu/installers/gui/03_fonts.sh
@@ -8,7 +8,9 @@ if [[ -n "${DIS_INSTALL:-}" ]]; then
   # Make fonts.sh available as a standalone tool in the user's shell.
   # DIS_PKG_ROOT is expanded now (at install time) so the absolute path is baked
   # into ~/.bashrc for future shell sessions.
-  bashrc_path_add "Ubuntu tools" "export PATH=\"$DIS_PKG_ROOT/bin:\$PATH\""
+  dis tools add-rc-path \
+    --name 'Ubuntu tools' \
+    --content "export PATH=\"$DIS_PKG_ROOT/bin:\$PATH\""
   # Also add it to PATH for the remainder of this installer session.
   export PATH="$DIS_PKG_ROOT/bin:$PATH"
 
